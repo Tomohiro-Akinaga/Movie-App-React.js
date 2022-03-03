1b22a5b5d0b48ebcbfdd053dd5d75a1bdacd38e6
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-03-02 17:09:32 -0800

    done laotop size header

--- src/components/Header-Favorite/Favorite.jsx
+++ src/components/Header-Favorite/Favorite.jsx
@@ -1,11 +1,11 @@
 import FavoriteStyle from "./Favorite.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faList } from "@fortawesome/free-solid-svg-icons";
 
 function Favorite() {
     return (
         <button className={FavoriteStyle.favoriteButton} type="button">
-            <FontAwesomeIcon icon={faBookmark} className={FavoriteStyle.bookmark}/>
+            <FontAwesomeIcon icon={faList} className={FavoriteStyle.bookmark}/>
         </button>
     )
 }
