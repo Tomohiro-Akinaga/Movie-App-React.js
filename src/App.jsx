8bea1f5d5c2dc12fca61edd455b6b9a9c40d20e2
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-03-03 17:30:12 -0800

    deployed in firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import AppStyle from "./App.module.scss";
 import Header from "./components/Header/Header.jsx";
 
+/* Firebase */
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+/* Firebase */
+const firebaseConfig = {
+  apiKey: "AIzaSyAwCDWhZpSWS1AHdverMYW_zcHsIOToTR4",
+  authDomain: "netflix-clone-react-app-2baad.firebaseapp.com",
+  projectId: "netflix-clone-react-app-2baad",
+  storageBucket: "netflix-clone-react-app-2baad.appspot.com",
+  messagingSenderId: "1009751770558",
+  appId: "1:1009751770558:web:91294d85443ef94c18a5ef",
+  measurementId: "G-K7JNJCMW6K"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+/* App component */
 function App() {
   return (
-    <>
       <Header />
-    </>
   )
 }
 
