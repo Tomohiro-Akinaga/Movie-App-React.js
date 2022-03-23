8d0fae3544f1dce8b8cfc88e5cade7f368228d3c
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-03-23 10:30:34 -0700

    done header before responsive

--- src/components/Home/Header/Header-Account/Account.jsx
+++ src/components/Home/Header/Header-Account/Account.jsx
@@ -1,3 +1,14 @@
+import AccountStyle from "./Account.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+
 export default function Account() {
-    return <p>Account</p>;
+    return (
+        <button className={AccountStyle.accountButton} type="button">
+            <FontAwesomeIcon
+                icon={faCircleUser}
+                className={AccountStyle.buttonIcon}
+            />
+        </button>
+    );
 }
